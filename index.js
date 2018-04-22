21189922f70f39c8004be26d3fd7b3fec33498e7
henriktrue <henriktrue@gmail.com>
2018-04-22 13:04:48 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var socket = require('socket.io');
+
+//App setup
+var app = express();
+var server = app.listen(4000, function(){
+  console.log('listening to requests on port 4000');
+});
+
+// Static files
+app.use(express.static('public'));
+
+//socket setup
+var io = socket(server);
+
+io.on('connection', function(socket){
+  console.log('connection established', socket.id)
+
+  //listen for events on motor 1 buttons
+  socket.on('m1_l', function(data){
+    io.sockets.emit('m1_l', data);
+  });
+
+  socket.on('m1_r', function(data){
+    io.sockets.emit('m1_r', data);
+  });
+
+  //listen for events on motor 2 buttons
+  socket.on('m2_l', function(data){
+    io.sockets.emit('m2_l', data);
+  });
+
+  socket.on('m2_r', function(data){
+    io.sockets.emit('m2_r', data);
+  });
+
+  //listen for events on motor 3 buttons
+  socket.on('m3_l', function(data){
+    io.sockets.emit('m3_l', data);
+  });
+
+  socket.on('m3_r', function(data){
+    io.sockets.emit('m3_r', data);
+  });
+
+  //listen for events on motor 4 buttons
+  socket.on('m4_l', function(data){
+    io.sockets.emit('m4_l', data);
+  });
+
+  socket.on('m4_r', function(data){
+    io.sockets.emit('m4_r', data);
+  });
+
+  //listen for events on motor 5 buttons
+  socket.on('m5_l', function(data){
+    io.sockets.emit('m5_l', data);
+  });
+
+  socket.on('m5_r', function(data){
+    io.sockets.emit('m5_r', data);
+  });
+});
---
+++ public/motors.js
@@ -0,0 +1,76 @@
+// Make connection
+var socket = io.connect('https://pro4team2.herokuapp.com/:4000');
+
+// Query DOM
+var message = document.getElementById('message'),
+      btn1_l = document.getElementById('send1_l'),
+      btn1_r = document.getElementById('send1_r'),
+      btn2_l = document.getElementById('send2_l'),
+      btn2_r = document.getElementById('send2_r'),
+      btn3_l = document.getElementById('send3_l'),
+      btn3_r = document.getElementById('send3_r'),
+      btn4_l = document.getElementById('send4_l'),
+      btn4_r = document.getElementById('send4_r'),
+      btn5_l = document.getElementById('send5_l'),
+      btn5_r = document.getElementById('send5_r'),
+      output = document.getElementById('output');
+
+// Emit events on motor1
+btn1_l.addEventListener('click', function(){
+  socket.emit('m1_l', {
+  });
+});
+
+// Emit events on motor1
+btn1_r.addEventListener('click', function(){
+  socket.emit('m1_r', {
+  });
+});
+
+// Emit events on motor2
+btn2_l.addEventListener('click', function(){
+  socket.emit('m2_l', {
+  });
+});
+
+// Emit events on motor2
+btn2_r.addEventListener('click', function(){
+  socket.emit('m2_r', {
+  });
+});
+
+// Emit events on motor3
+btn3_l.addEventListener('click', function(){
+  socket.emit('m3_l', {
+  });
+});
+
+// Emit events on motor3
+btn3_r.addEventListener('click', function(){
+  socket.emit('m3_r', {
+  });
+});
+
+// Emit events on motor4
+btn4_l.addEventListener('click', function(){
+  socket.emit('m4_l', {
+  });
+});
+
+// Emit events on motor4
+btn4_r.addEventListener('click', function(){
+  socket.emit('m4_r', {
+  });
+});
+
+// Emit events on motor5
+btn5_l.addEventListener('click', function(){
+  socket.emit('m5_l', {
+  });
+});
+
+// Emit events on motor5
+btn5_r.addEventListener('click', function(){
+  socket.emit('m5_r', {
+  });
+});
